b8db1607c15e1a49d7988a038caab516e43c8523
LongNgoo <107351915+LongNgoo@users.noreply.github.com>
2023-06-02 15:02:35 +0700

    Long 4 bang

---
+++ src/main/java/com/fpoly/iocare/model/RiskClassification.java
@@ -0,0 +1,27 @@
+package com.fpoly.iocare.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "RiskClassification")
+public class RiskClassification {
+	@Id
+	@Column(name = "Riskclassificationid")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer riskClassificationId;
+	
+	@Column(name = "Riskclassificationdes")
+	private String riskClassificationDes;
+}
