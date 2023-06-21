7884b5173c77f3f1d4bcdda7baf091c5dd86e353
lbachannel <anlb19749@gmail.com>
2023-06-21 09:15:46 +0700

    upload

--- src/main/resources/static/assets/admin/js/import-excel.js
+++ src/main/resources/static/assets/admin/js/import-excel.js
@@ -249,6 +249,10 @@ app.controller("ctrl-import", function($scope, $http) {
 	$scope.load_semester();
 	$scope.load_campaign();
 	$scope.load_employees();
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
   
 /*--Phân công form--*/
 
@@ -326,7 +330,11 @@ $scope.updateSelectedStudents = function(student) {
   $scope.sortTableDataAssigned();
 };
 };
+<<<<<<< Updated upstream
 	
+=======
+
+>>>>>>> Stashed changes
 	// Thêm một mảng selectedStudents để lưu trạng thái chọn của sinh viên
 $scope.selectedStudents = [];
 
